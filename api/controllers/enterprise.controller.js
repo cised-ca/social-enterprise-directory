a2e8ed26029532614c040d5a243407567d863f18
chad-g-adams <chad.adams@protonmail.com>
2016-07-13 16:20:17 -0400

    Initial backend running off hard-coded json file

--- api/controllers/enterprise.controller.js
+++ api/controllers/enterprise.controller.js
@@ -7,8 +7,10 @@ module.exports.getAll = function(req, res) {
 
 module.exports.getOne = function(req, res) {
   var id = req.params.enterpriseId;
-  var index = parseInt(id) - 1;
+  enterprise = enterpriseData["enterprises"].filter(function(enterprise) {
+    return enterprise._id == id;
+  })[0];
 
   res.status(200)
-    .json(enterpriseData["enterprises"][index]);
+    .json(enterprise);
 }
