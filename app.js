53289aa0c472dad68729ebd3bc5e2eaa384a8133
Chimo <chimo@chromic.org>
2016-07-13 20:38:42 -0400

    Allow CORS requests from all domains (*)
    Since this is an API after all

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var routes = require('./api/routes');
 
 app.set('port', 14000);
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use('/api/', routes);
 
 app.listen(app.get('port'), function() {
