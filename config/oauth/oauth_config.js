2eb25423db87a31821935aa4596cc132ced21a32
chad-g-adams <chad.adams@protonmail.com>
2017-04-09 16:47:48 -0400

    Use proper express session store

--- config/oauth/oauth_config.js
+++ config/oauth/oauth_config.js
@@ -3,25 +3,25 @@ const convict = require('convict');
 let oauthConfig = convict({
   redirectURLOnSuccessfulLogin: {
     doc: 'The URL to load after oauth succeeds',
-    format: String,
+    format: 'url',
     default: '',
     env: 'OAUTH_REDIRECT_URL'
   },
   twitterCallbackURL: {
     doc: 'The twitter callback URL after oauth succeeds',
-    format: String,
+    format: 'url',
     default: '',
     env: 'TWITTER_CALLBACK_URL'
   },
   facebookCallbackURL: {
     doc: 'The facebook callback URL after oauth succeeds',
-    format: String,
+    format: 'url',
     default: '',
     env: 'FACEBOOK_CALLBACK_URL'
   },
   instagramCallbackURL: {
     doc: 'The instagram callback URL after oauth succeeds',
-    format: String,
+    format: 'url',
     default: '',
     env: 'INSTAGRAM_CALLBACK_URL'
   },
@@ -29,37 +29,43 @@ let oauthConfig = convict({
     doc: 'The twitter consumer key',
     format: String,
     default: '',
-    env: 'TWITTER_CONSUMER_KEY'
+    env: 'TWITTER_CONSUMER_KEY',
+    sensitive: true
   },
   twitterConsumerSecret: {
     doc: 'The twitter consumer secret',
     format: String,
     default: '',
-    env: 'TWITTER_CONSUMER_SECRET'
+    env: 'TWITTER_CONSUMER_SECRET',
+    sensitive: true
   },
   facebookClientId: {
     doc: 'The facebook client id',
     format: String,
     default: '',
-    env: 'FACEBOOK_CLIENT_ID'
+    env: 'FACEBOOK_CLIENT_ID',
+    sensitive: true
   },
   facebookSecret: {
     doc: 'The facebook secret',
     format: String,
     default: '',
-    env: 'FACEBOOK_SECRET'
+    env: 'FACEBOOK_SECRET',
+    sensitive: true
   },
   instagramClientId: {
     doc: 'The instagram client id',
     format: String,
     default: '',
-    env: 'INSTAGRAM_CLIENT_ID'
+    env: 'INSTAGRAM_CLIENT_ID',
+    sensitive: true
   },
   instagramSecret: {
     doc: 'The instagram secret',
     format: String,
     default: '',
-    env: 'INSTAGRAM_SECRET'
+    env: 'INSTAGRAM_SECRET',
+    sensitive: true
   }
 });
 
