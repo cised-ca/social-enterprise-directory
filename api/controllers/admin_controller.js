f33882d0b97381d50a62748fe6b0db63ba8c2001
chad-g-adams <chad.adams@protonmail.com>
2017-09-16 14:53:22 -0400

    adding directory admin endpoints

--- api/controllers/admin_controller.js
+++ api/controllers/admin_controller.js
@@ -13,6 +13,51 @@ module.exports.getAllDirectoryAdmins = function(req, res) {
     });
 };
 
+module.exports.updateDirectoryAdmins = function(req, res) {
+  let newAdmins = req.swagger.params.admins.value;
+
+  if (newAdmins.length === 0) {
+    res.status(400).json({'message': 'Must have at least one enterprise admin'});
+    return;
+  }
+
+  // For stability reasons we don't ever want to delete all admins at any point.
+  // Therefore, we'll compare the new admin list with what's in the DB and
+  //  1. Add any new admins not currently in DB
+  //  2. Then delete old admins that are not in the new list
+
+  let adminsToAdd = [];
+  let adminsToDelete = [];
+
+  directoryAdminModel.find()
+  .then(dbAdmins => {
+    adminsToAdd = newAdmins.filter(admin => {
+      return dbAdmins.filter(dbAdmin => admin.email === dbAdmin.email).length === 0;
+    });
+    adminsToDelete = dbAdmins.filter(dbAdmin => {
+      return newAdmins.filter(newAdmin => newAdmin.email === dbAdmin.email).length === 0;
+    });
+    return Promise.resolve();
+  })
+  .then( () => {
+    return Promise.all(adminsToAdd.map(admin => {
+      return directoryAdminModel.create(admin);
+    }));
+  })
+  .then( () => {
+    return Promise.all(adminsToDelete.map(admin => {
+      return directoryAdminModel.deleteOne({'email': admin.email});
+    }));
+  })
+  .then(() => {
+    res.status(200).json({});
+  })
+  .catch(err => {
+    logger.error('Error updating directory administrators ' + err);
+    res.status(500).json({'message': err});
+  });
+};
+
 function processDBAdminResults(res, dbAdmins) {
   let apiAdmins = dbAdmins.map((dbAdmin) => {
     return { email: dbAdmin.email };
